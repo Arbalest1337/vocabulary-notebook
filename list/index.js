8537ae6e5689c318470b8d62edacd71b3322e12e
Arbalest-Dcmk <499624095@qq.com>
2022-05-17 22:48:15 +0800

    feat: update

--- list/index.js
+++ list/index.js
@@ -1,13 +1,22 @@
 const container = document.getElementById('list')
 async function refresh() {
-    let list = await storageGet('vocabularies')
-    const result = []
-    list.reverse().forEach(item => {
-        
-    });
-    
-    container.innerHTML = `<div class="time-block" data-date="2022.5.17">
-    <span class="vocab" title="中文23">ok</span>
-</div>`
+    const result = await storageGet('vocabularies') || {}
+    const splitByDate = {}
+    result.reverse().forEach(item => {
+        const date = new Date(item.timestamp).toLocaleDateString('zh-CN').replace(/\//g, '.')
+        if (!splitByDate[date]) { splitByDate[date] = [] }
+        splitByDate[date].push({ ...item, date })
+    })
+
+    let nodesStr = ``
+    for (let key in splitByDate) {
+        let vocabulariesStr = ``
+        for (let item of splitByDate[key]) {
+            vocabulariesStr += `<span class="vocab" title="${item.translated}">${item.original}</span>`
+        }
+        nodesStr += `<div class="time-block" data-date="${key}">${vocabulariesStr}</div>`
+    }
+
+    container.innerHTML = nodesStr
 }
 refresh()
