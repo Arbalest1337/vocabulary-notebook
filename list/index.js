6227064a095b0e2c862eb190b4d0b2f6ae2cb28f
Arbalest <499624095@qq.com>
2022-05-18 12:09:00 +0800

    feat: show other means

--- list/index.js
+++ list/index.js
@@ -1,10 +1,12 @@
 const container = document.getElementById('list')
 async function refresh() {
-    const result = await storageGet('vocabularies') || {}
+    const result = (await storageGet('vocabularies')) || {}
     const splitByDate = {}
     result.reverse().forEach(item => {
         const date = new Date(item.timestamp).toLocaleDateString('zh-CN').replace(/\//g, '.')
-        if (!splitByDate[date]) { splitByDate[date] = [] }
+        if (!splitByDate[date]) {
+            splitByDate[date] = []
+        }
         splitByDate[date].push({ ...item, date })
     })
 
@@ -12,7 +14,11 @@ async function refresh() {
     for (let key in splitByDate) {
         let vocabulariesStr = ``
         for (let item of splitByDate[key]) {
-            vocabulariesStr += `<span class="vocab" title="${item.translated}">${item.original}</span>`
+            let allMeans = item.translated
+            for (let mean of item.otherMeans) {
+                allMeans += `\n\n${mean.type}: ${mean.value}`
+            }
+            vocabulariesStr += `<span class="vocab" title="${allMeans}">${item.original}</span>`
         }
         nodesStr += `<div class="time-block" data-date="${key}">${vocabulariesStr}</div>`
     }
